e7cd64b253df856d883a5603774c471b48ad6c92
Vladimir Kirpichev <djbobah@mail.ru>
2021-09-08 14:15:21 +0300

    init

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Heloo World, ReactJS!</p>
+        <p>Hello World, ReactJS!</p>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,11 +1,48 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+//const el = React.createElement("h1", null, "Hello World, ReactJS");
+
+const AppList = () => {
+  const items = ["item 1", "item 2", "item 3", "item 4", "item 5"];
+  const firstItems = <li>Item 0</li>;
+
+  const isAuth = false;
+  return (
+    <ul>
+      {isAuth ? firstItems : null}
+      {items.map((item) => (
+        <li>{item}</li>
+      ))}
+      <li>{items[0]}</li>
+      <li>{items[1]}</li>
+    </ul>
+  );
+};
+
+const AppHeader = () => {
+  return <h1>This is my header!</h1>;
+};
+
+const AppInput = () => {
+  const placeholder = "Type text...";
+  return (
+    <label>
+      <input placeholder={placeholder} />
+    </label>
+  );
+};
+
+const App = () => {
+  return (
+    <>
+      <AppHeader />
+      <AppInput />
+      <AppList />
+      <AppHeader />
+      <AppList />
+    </>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
